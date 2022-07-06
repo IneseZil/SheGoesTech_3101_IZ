31fe7474f734efd54da1035b506d9cd29fe22a01
IneseZil <i.zilinska.lv@gmail.com>
2022-07-06 21:32:05 +0300

    Initial 1st commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int daysinshegoestech = 3;
+        switch (daysinshegoestech) {
+            case 1:
+                System.out.println("Just started");
+                break;
+            case 30:
+                System.out.println("Keep going!");
+                break;
+            case 60:
+                System.out.println("2/3rds passed!");
+                break;
+            case 90:
+                System.out.println("Almost there!!!");
+                break;
+            default:
+                System.out.println("Somewhere in the middle :)");
+                    }
+    }
+}
---
+++ src/SecondLesson.java
@@ -0,0 +1,12 @@
+public class SecondLesson {
+        public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Here is me");
+        String MyCourse ="learning SheGoesTechJAVA";
+        System.out.println(MyCourse);
+        Integer DaysPassed=3;
+        Integer DaysTotal=90;
+        System.out.println("Days left"+(DaysTotal-DaysPassed));
+
+    }
+}
