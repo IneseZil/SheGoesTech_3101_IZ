1a449a0346d455f7b63ce48416a34d02b3bfd8c0
IneseZil <IneseZil@github.com>
2022-07-21 16:52:54 +0300

    .

---
+++ src/HW3_2207.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HW3_2207 {
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter any number");
+            int input = scanner.nextInt();
+            if (input >= 0) {
+                do {
+                    System.out.print(input+" ");
+                } while (input-- != 0);
+            }else
+                do {
+                    System.out.print(input+" ");
+                } while (input++ != 0);
+        }
+}
+
