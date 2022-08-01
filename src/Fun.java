b3648c71c383d5c4a92095c7dabb8b3d237613d8
IneseZil <108826529+IneseZil@users.noreply.github.com>
2022-08-01 21:35:58 +0300

    Delete Fun.java

--- src/Fun.java
+++
@@ -1,8 +0,0 @@
-public class Fun {
-    public static void main(String[] args) {
-        System.out.println("It is Day5");
-        int GoodQuestions = 1;
-        int StupidQuestions = 111;
-        System.out.println("Are they idiots? "+(GoodQuestions<StupidQuestions));
-    }
-}
