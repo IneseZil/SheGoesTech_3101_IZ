84d835cb72f49b30a45753c491d9d7059611516c
IneseZil <IneseZil@github.com>
2022-08-10 21:33:47 +0300

    updated LeapYear

--- src/ClassWork/MethodLeapYear.java
+++ src/ClassWork/MethodLeapYear.java
@@ -9,15 +9,31 @@ public class MethodLeapYear {
         int year = scanner.nextInt();
 
         determineLeapYear(year);
+        determineLeapYear(2039);
+        determineLeapYear(1827);
+        determineLeapYear(1973);
+
+        String result = determineLeapYearRet(year);
+        System.out.println(result);
     }
-    public static void determineLeapYear(int year){
+
+    public static void determineLeapYear(int year) {
         if (year % 400 == 0) {
             System.out.println(year + " is a Leap year");
         } else if (year % 4 == 0) {
             System.out.println(year + " is a Leap year");
         } else if (year % 100 == 0) {
             System.out.println(year + " is NOT a Leap year");
-        } else {System.out.println(year + " is NOT a Leap year");
+        } else {
+            System.out.println(year + " is NOT a Leap year");
+        }
+    }
+
+    public static String determineLeapYearRet(int year) {    //use return multiple times. Return exits a method
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            return year+" is a Leap year ^^";
+        } else {
+            return year+" is NOT a Leap year ^^";
         }
     }
 }
---
+++ src/Homework/HW2_1208_MinMaxMethods.java
@@ -0,0 +1,14 @@
+package Homework;
+
+import java.util.Arrays;
+
+public class HW2_1208_MinMaxMethods {
+    public static void main(String[] args) {
+
+        int [] my_array = {1,67,98,76,97,99,97};
+        System.out.println("Original array :"+ Arrays.toString(my_array));
+        //System.out.println("Maximum value is"+ integer);
+        //System.out.println("Minimum value is"+ integer);
+
+    }
+}
