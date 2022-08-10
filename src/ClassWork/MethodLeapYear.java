86bb7f761dc48ee074d7f5e0843227c6f825a04f
IneseZil <IneseZil@github.com>
2022-08-10 20:43:46 +0300

    OOP1

---
+++ src/ClassWork/MethodLeapYear.java
@@ -0,0 +1,24 @@
+package ClassWork;
+
+import java.util.Scanner;
+
+public class MethodLeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a year");
+        int year = scanner.nextInt();
+
+        determinLeapYear(year);
+
+    }
+    public static void determinLeapYear(int year){
+        if (year % 400 == 0) {
+            System.out.println(year + " is a Leap year");
+        } else if (year % 4 == 0) {
+            System.out.println(year + " is a Leap year");
+        } else if (year % 100 == 0) {
+            System.out.println(year + " is NOT a Leap year");
+        } else {System.out.println(year + " is NOT a Leap year");
+        }
+    }
+}
