8f3216920346dcdb0b4797c920f2ad1e3ad683e8
IneseZil <IneseZil@github.com>
2022-08-11 21:23:11 +0300

    MinMaxArray method

--- src/Homework/HW2_1208_MinMaxMethods.java
+++ src/Homework/HW2_1208_MinMaxMethods.java
@@ -5,10 +5,28 @@ import java.util.Arrays;
 public class HW2_1208_MinMaxMethods {
     public static void main(String[] args) {
 
-        int [] my_array = {1,67,98,76,97,99,97};
+        int [] my_array = {1,67,98,76,97,99,97,234,123,3656,2312,-312,31,3454,3};
         System.out.println("Original array :"+ Arrays.toString(my_array));
-        //System.out.println("Maximum value is"+ integer);
-        //System.out.println("Minimum value is"+ integer);
 
+        System.out.println("Maximum value for the array = " + maxMeth(my_array));
+        System.out.println("Minimum value for the array = " + minMeth(my_array));
+    }
+    public static int maxMeth(int [] my_array) {
+        int max = my_array[0];
+        for (int i : my_array) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
+    public static int minMeth(int [] my_array) {
+        int min = my_array[0];
+        for (int i : my_array) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
     }
 }
