73dce58d9acd2c0c752f4bfc745167e5e1dfe836
IneseZil <IneseZil@github.com>
2022-08-19 20:58:04 +0300

    abstract

--- src/OOP/EnumExmpl.java
+++ src/OOP/EnumExmpl.java
@@ -3,10 +3,19 @@ package oop;
 enum Level {
     HIGH, LOW, MEDIUM
 }
-
 public class EnumExmpl {
     public static void main(String[] args) {
         Level myLevel = Level.HIGH;
+        switch (myLevel){
+            case LOW:
+                System.out.println("Level: LOW");
+                break;
+            case MEDIUM:
+                System.out.println("Level: MEDIUM");
+            case HIGH:
+                System.out.println("Level: HIGH");
+                break;
+        }
 
 
 
